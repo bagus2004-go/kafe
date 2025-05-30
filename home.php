85e130e990523a66199bdaf8bc7f0701ad4cd04b
bagus2004-go <bs130408@gmail.com>
2025-05-30 07:53:17 +0700

    nambah img + atur css

--- home.php
+++ home.php
@@ -80,28 +80,102 @@
         <section class="thumb">
             <div class="box-container">
                 <div class="box">
-                    <img src="img/">
+                    <img src="img/thumb1.jpg">
                     <h3>Kopi Panas</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
                     <i class="bx bx-chevron-right"></i>
                 </div>
                 <div class="box">
-                    <img src="img/">
+                    <img src="img/thumb2.jpg">
                     <h3>Es Kopi</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
                     <i class="bx bx-chevron-right"></i>
                 </div>
                 <div class="box">
-                    <img src="img/">
+                    <img src="img/thumb.jpg">
                     <h3>Roti</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
                     <i class="bx bx-chevron-right"></i>
                 </div>
                 <div class="box">
-                    <img src="img/">
+                    <img src="img/thumb3.jpg">
                     <h3>Biji Kopi</h3>
                     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
                     <i class="bx bx-chevron-right"></i>
+                </div>
+            </div>
+        </section>
+        <section class="container">
+            <div class="box-container">
+                <div class="box">
+                    <img src="img/about-us.jpg">
+                </div>
+                <div class="box">
+                    <img src="img/download.png">
+                    <span>kopi</span>
+                    <h1>hemat sampai 50%</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+        </section>
+        <section class="shop">
+            <div class="title">
+                <img src="img/download.png">
+                <h1>Produk Terlaris</h1>
+            </div>
+            <div class="row">
+                <img src="img/about.jpg">
+                <div class="row-detail">
+                    <img src="img/basil.jpg">
+                    <div class="top-footer">
+                        <h1>Segelas kopi untuk awali hari</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="box-container">
+                <div class="box">
+                    <img src="img/card.jpg">
+                    <a href="view_products.php" class="btn">beli sekarang</a>
+                </div>
+                <div class="box">
+                    <img src="img/card1.jpg">
+                    <a href="view_products.php" class="btn">beli sekarang</a>
+                </div>
+                <div class="box">
+                    <img src="img/card2.jpg">
+                    <a href="view_products.php" class="btn">beli sekarang</a>
+                </div>
+                <div class="box">
+                    <img src="img/card3.jpg">
+                    <a href="view_products.php" class="btn">beli sekarang</a>
+                </div>
+                <div class="box">
+                    <img src="img/card4.jpg">
+                    <a href="view_products.php" class="btn">beli sekarang</a>
+                </div>
+                <div class="box">
+                    <img src="img/card5.jpg">
+                    <a href="view_products.php" class="btn">beli sekarang</a>
+                </div>
+            </div>
+        </section>
+        <section class="shop-category">
+            <div class="box-container">
+                <div class="box">
+                    <img src="img/1.jpg">
+                    <div class="detail">
+                        <span>PENAWARAN BESAR</span>
+                        <h1>ekstra diskon 15%</h1>
+                        <a href="view_products.php" class="btn">beli sekarang</a>
+                    </div>
+                </div>
+                <div class="box">
+                    <img src="img/2.jpg">
+                    <div class="detail">
+                        <span>Rasa yang baru</span>
+                        <h1>Kopi rumahan</h1>
+                        <a href="view_products.php" class="btn">beli sekarang</a>
+                    </div>
                 </div>
             </div>
         </section>
