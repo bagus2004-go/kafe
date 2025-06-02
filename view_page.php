50ed341bb92e4a7d906493743d61f97818b75421
bagus2004-go <bs130408@gmail.com>
2025-06-02 09:53:11 +0700

    nambah cart + wishlist (masih ada bug di wishlist)

--- view_page.php
+++ view_page.php
@@ -16,7 +16,7 @@
     if (isset($_POST['add_to_wishlist'])) {
         $id = unique_id();
         $product_id = $_POST['product_id'];
-        $varify_wishlist = $conn->prepare("SELECT * FROM `whislist` WHERE user_id = ? AND product_id = ?");
+        $varify_wishlist = $conn->prepare("SELECT * FROM `wishlist` WHERE user_id = ? AND product_id = ?");
         $varify_wishlist->execute([$user_id, $product_id]);
         $cart_num = $conn->prepare("SELECT * FROM `cart` WHERE user_id = ? AND product_id = ?");
         $cart_num->execute([$user_id, $product_id]);
@@ -28,7 +28,7 @@
             $select_price = $conn->prepare("SELECT * FROM `products` WHERE id = ? LIMIT 1");
             $select_price->execute([$product_id]);
             $fetch_price = $select_price->fetch(PDO::FETCH_ASSOC);
-            $insert_wishlist = $conn->prepare("INSERT INTO `whislist` (id, user_id, product_id, price) VALUES(?,?,?,?)");
+            $insert_wishlist = $conn->prepare("INSERT INTO `wishlist` (id, user_id, product_id, price) VALUES(?,?,?,?)");
             $insert_wishlist->execute([$id, $user_id, $product_id, $fetch_price['price']]);
             $success_msg[] = 'produk berhasil dimasukkan ke daftar anda';
         }
